68f7558c6084500fd5df1ebf8e5fd5692211c2cd
nonwander <nemykin.eu@yandex.ru>
2021-08-03 13:12:59 +0300

    Seventh commit: Fixed syntax mistakes

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+
 from django.test import Client, TestCase
 
 
@@ -10,14 +11,12 @@ class StaticPagesURLTests(TestCase):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('/second/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('/second/')
         self.assertContains(response, 'А это вторая страница!')
